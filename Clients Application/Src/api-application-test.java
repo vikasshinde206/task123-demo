da06e883e047d220174ec9824dbae4918ed08515
Vikas Shinde <126188021+vikasshinde206@users.noreply.github.com>
2025-11-06 22:15:19 +0530

    Add initial test for ClientsApiApplication

---
+++ Clients Application/Src/api-application-test.java
@@ -0,0 +1,11 @@
+package com.deltacapita.clientsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientsApiApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
